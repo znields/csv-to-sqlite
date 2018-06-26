e8d82e46a16bac133ed1702c9c1b6ffdef83cdbe
Isaiah Nields <imnields@gmail.com>
2018-06-26 14:11:43 -0400

    Added SQL Script

--- renderer.js
+++ renderer.js
@@ -1,3 +1,29 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+//
+
+function convert() {
+
+    const { spawn } = require('child_process');
+    const ls = spawn('python', ['python/import_db.py', document.getElementById('file_location').value]);
+
+    ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
+
+    ls.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+
+}
+
+
+
+
+// const {dialog} = require('electron').remote
+// console.log(dialog.showOpenDialog({properties: ['openFile']}));
